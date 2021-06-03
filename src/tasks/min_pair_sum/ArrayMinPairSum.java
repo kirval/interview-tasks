bbe410a40891c5e439723ae5c16a496f1310a0eb
vkir <valyushko.kirill@gmail.com>
2021-06-03 15:39:27 +0300

    add `Minimal pair sum` task

---
+++ src/tasks/min_pair_sum/ArrayMinPairSum.java
@@ -0,0 +1,34 @@
+package tasks.min_pair_sum;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class ArrayMinPairSum {
+
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        int sum = 0;
+        list.add(5);
+        list.add(8);
+        list.add(2);
+        list.add(3);
+
+        if (list.size() == 1) {
+            sum += list.get(0);
+        } else if (list.size() > 1) {
+            System.out.println(list);
+            while (list.size() >= 2) {
+                Collections.sort(list);
+                Integer minSum = list.get(0) + list.get(1);
+                list.remove(0);
+                list.remove(0);
+                list.add(minSum);
+                sum += minSum;
+                System.out.println(list);
+            }
+        }
+        System.out.println("\nMinimal sum = " + sum);
+    }
+
+}
---
+++ src/tasks/min_pair_sum/PriorityQueueMinPairSum.java
@@ -0,0 +1,32 @@
+package tasks.min_pair_sum;
+
+import java.util.PriorityQueue;
+
+public class PriorityQueueMinPairSum {
+
+    public static void main(String[] args) {
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        int sum = 0;
+        queue.offer(5);
+        queue.offer(8);
+        queue.offer(2);
+        queue.offer(3);
+
+        if (queue.size() == 1) {
+            System.out.println(queue);
+            sum += queue.peek();
+            System.out.println("Sum = " + sum);
+        } else if (queue.size() > 1) {
+            System.out.println(queue);
+            while (queue.size() >= 2) {
+                int minSum = queue.poll() + queue.poll();
+                queue.offer(minSum);
+                sum += minSum;
+                System.out.println(queue);
+            }
+        }
+        System.out.println("\nMinimal sum = " + sum);
+
+    }
+
+}
