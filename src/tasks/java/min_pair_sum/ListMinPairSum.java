ecead8df1efe8a8a93fa4e0dece4872bf9e062d9
vkir <valyushko.kirill@gmail.com>
2021-06-04 11:08:14 +0300

    increase performance of "Minimal pair sum" list solution

--- src/tasks/java/min_pair_sum/ListMinPairSum.java
+++ src/tasks/java/min_pair_sum/ListMinPairSum.java
@@ -1,13 +1,13 @@
 package tasks.java.min_pair_sum;
 
-import java.util.ArrayList;
 import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
-public class ArrayMinPairSum {
+public class ListMinPairSum {
 
     public static void main(String[] args) {
-        List<Integer> list = new ArrayList<>();
+        List<Integer> list = new LinkedList<>();
         int sum = 0;
         list.add(5);
         list.add(8);
@@ -20,7 +20,7 @@ public class ArrayMinPairSum {
             System.out.println(list);
             while (list.size() >= 2) {
                 Collections.sort(list);
-                Integer minSum = list.get(0) + list.get(1);
+                int minSum = list.get(0) + list.get(1);
                 list.remove(0);
                 list.remove(0);
                 list.add(minSum);
